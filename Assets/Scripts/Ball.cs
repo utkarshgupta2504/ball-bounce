890b074bcfa846f3ed783ded714f74f904d4941f
Utkarsh Gupta <utkarshgupta2504@gmail.com>
2020-08-06 00:00:04 +0530

    Completed App

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float bounceForce;
+    bool gameStarted;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameStarted)
+        {
+            if (Input.anyKeyDown)
+            {
+                startBounce();
+                gameStarted = true;
+                GameManager.instance.gameStart();
+            }
+        }
+    }
+
+    void startBounce()
+    {
+
+        Vector2 randomDirection = new Vector2((float)Random.Range(-10000, 10000) / 10000, 1);
+
+        if (randomDirection.x == 0)
+        {
+            randomDirection.x = 1;
+        }
+
+        rb.AddForce(randomDirection * bounceForce, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //checks for collsion
+
+        if (other.gameObject.tag == "Fall")
+        {
+            //checks if the colliding body has the tag of FallCheck
+            GameManager.instance.restart();
+        }
+
+        else if (other.gameObject.tag == "Paddle")
+        {
+            GameManager.instance.scoreUp();
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    int score;
+    public Text scoreText;
+    public GameObject gameStartUI;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void scoreUp()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    public void gameStart()
+    {
+        gameStartUI.SetActive(false);
+        scoreText.gameObject.SetActive(true);
+    }
+}
